0c00ddb30f285174012c1b7fe1b8036022c531a4
AlaaHamdyMohammadi <alaahamdy2197@gmail.com>
2023-11-18 22:29:59 +0200

    Getting Data From Database

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import MeetupList from "@/components/meetups/MeetupList";
+import { MongoClient } from "mongodb";
 
 const DUMMY_DATA = [
   {
@@ -10,12 +11,39 @@ const DUMMY_DATA = [
   { id: "m2", image: "", title: "A Second Meetup", address: "Some address 5" },
 ];
 
-function HomePage() {
+function HomePage(props) {
+  const {meetups} = props
   return (
     <div>
-      <MeetupList meetups={DUMMY_DATA} />
+      <MeetupList meetups={meetups} />
     </div>
   );
+}
+
+export async function getStaticProps(){
+   const client = await MongoClient.connect(
+     `mongodb+srv://alaahamdy2197:S3UZjXd4wpGjDtm2@cluster0.qmymzal.mongodb.net/meetups?retryWrites=true&w=majority`
+   );
+
+   const db = client.db();
+   const meetupsCollection = db.collection("meetups");
+
+   const meetups = await meetupsCollection.find().toArray();
+
+   client.close();
+
+   return {
+     props: {
+       meetups: meetups.map((meetup) => ({
+         title: meetup.title,
+         address: meetup.address,
+         image: meetup.image,
+         id: meetup._id.toString(),
+       })),
+     },
+     revalidate: 1,
+   };
+  
 }
 
 export default HomePage;
